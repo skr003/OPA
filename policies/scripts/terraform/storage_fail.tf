8ee86644cde030a77e9a8b33f46affbe3957776f
skr003 <78012061+skr003@users.noreply.github.com>
2026-02-11 00:29:54 +0530

    Create storage_fail.tf

---
+++ policies/scripts/terraform/storage_fail.tf
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------
+# INSECURE STORAGE ACCOUNT (Will Fail tfsec/Checkov)
+# Violations:
+# 1. allow_blob_public_access is TRUE (CIS 3.1)
+# 2. enable_https_traffic_only is FALSE (CIS 3.2)
+# 3. min_tls_version is "TLS1_0" (Should be TLS1_2)
+# -------------------------------------------------------------
+
+resource "azurerm_storage_account" "bad_storage" {
+  name                     = "${var.prefix}badstore"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  # SECURITY FLAWS HERE:
+  allow_blob_public_access  = true          # <--- VIOLATION!
+  enable_https_traffic_only = false         # <--- VIOLATION!
+  min_tls_version           = "TLS1_0"      # <--- VIOLATION!
+  
+  tags = {
+    environment = "dev"
+    compliance  = "none"
+  }
+}
