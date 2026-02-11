fa9295f9de47a3222c70f862dcc6b94c3e0af065
skr003 <78012061+skr003@users.noreply.github.com>
2026-02-11 23:30:29 +0530

    Update storage_fail.tf

--- policies/scripts/terraform/storage_fail.tf
+++ policies/scripts/terraform/storage_fail.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "bad_storage" {
   # VIOLATION: PUBLIC ACCESS ENABLED
   # This triggers tfsec Rule AZU012 -> High Severity -> OPA BLOCK
   # -------------------------------------------------------------
-  allow_nested_items_to_be_public = true  # <--- The "Red Flag"
+  allow_nested_items_to_be_public = false  # <--- The "Red Flag"
 
   # VIOLATION: No Network Rules (Open to 0.0.0.0/0)
   public_network_access_enabled = true
